2d4e88babae47d2affec6a2f8ff7c26e0dbdc2a0
ActingCute酱 <rem486@qq.com>
2021-03-23 22:26:19 +0800

    1.适配下捉取的html数据 2.英雄列表和装备列表点击问题

--- weixin/pages/components/article/article.js
+++ weixin/pages/components/article/article.js
@@ -32,6 +32,14 @@ Component({
         success(res) {
           if (res.statusCode == 200 && res.data.Code == CODE.Success) {
             let { sContent, sTitle, sIdxTime } = res.data.Data.data;
+            sContent = sContent.replace(/<p([\s\w"=\/\.:;]+)((?:(style="[^"]+")))/ig, '<p')
+              .replace(/<p>/ig, '<p style="font-size: 19px;">')
+              .replace(/<img([\s\w"-=\/\.:;]+)((?:(height="[^"]+")))/ig, '<img$1')
+              .replace(/<img([\s\w"-=\/\.:;]+)((?:(width="[^"]+")))/ig, '<img$1')
+              .replace(/<img([\s\w"-=\/\.:;]+)((?:(style="[^"]+")))/ig, '<img$1')
+              .replace(/<img([\s\w"-=\/\.:;]+)((?:(alt="[^"]+")))/ig, '<img$1')
+              .replace(/<img([\s\w"-=\/\.:;]+)/ig, '<img style="width: 100%;" $1');
+            sContent = "<div style='width:95%;margin:0 auto'>" + sContent + "</div>";
             that.setData({
               sContent, sTitle, sIdxTime
             });
