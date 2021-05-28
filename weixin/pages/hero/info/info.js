d2bbeffc4e4866fc7ba09ab2f9999074971298a9
ActingCute酱 <rem486@qq.com>
2021-05-28 14:12:52 +0800

    1.英雄详情页

--- weixin/pages/hero/info/info.js
+++ weixin/pages/hero/info/info.js
@@ -1,14 +1,28 @@
 const app = getApp();
 const globalData = app.globalData;
-const { CODE, MSG, BASE_URL } = globalData;
+const { CODE, MSG, BASE_URL, NAV_DATA } = globalData;
 
 Page({
     data: {
-        info: {},
+        hero_type: "小可爱",
+        type_data: [
+            { key: 1, text: "战士" },
+            { key: 2, text: "法师" },
+            { key: 3, text: "坦克" },
+            { key: 4, text: "刺客" },
+            { key: 5, text: "射手" },
+            { key: 6, text: "辅助" }
+        ],
+        paddingTop: "48px",
+        info: null,
         loading: true,
         animated: true
     },
     onLoad: function (data) {
+        this.setData({
+            paddingTop: NAV_DATA.status + NAV_DATA.height + "px"
+        })
+        console.log("paddingTop -- ", this.data.paddingTop)
         let { id } = data;
         //标题
         app.globalData.SET_TITLE("英雄详情");
@@ -22,16 +36,32 @@ Page({
             header: {
                 'content-type': 'application/json'
             },
-            success(res) {
+            complete(c) {
                 that.setData({
                     animated: false,
                     loading: false
                 });
+            },
+            success(res) {
+
                 if (res.statusCode == 200 && res.data.Code == CODE.Success) {
+                    let ht = that.data.type_data.find((item) => item.key == res.data.Data.hero_type);
+                    let hero_type = "";
+                    if (ht) {
+                        hero_type = ht.text;
+                    }
+                    let hero_type2 = res.data.Data.hero_type2;
+                    if (hero_type2 && hero_type2 > 0) {
+                        ht = that.data.type_data.find((item) => item.key == hero_type2);
+                        if (ht) {
+                            hero_type += "/" + ht.text;
+                        }
+                    }
                     that.setData({
-                        info: res.data.Data
+                        info: res.data.Data,
+                        hero_type
                     })
-                    console.log(res.data.Data);
+                    console.log(that.data.info);
                 } else {
                     wx.hideToast();
                     wx.showToast({
@@ -40,12 +70,10 @@ Page({
                         duration: 2000
                     });
                 }
+
             },
             error(err) {
-                that.setData({
-                    animated: false,
-                    loading: false
-                });
+
                 wx.hideToast();
                 wx.showToast({
                     title: err,
